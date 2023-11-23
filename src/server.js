07886a62487d40a948b24fca451769969d42885b
Rodrigo <drigodeveloper@gmail.com>
2023-11-22 21:51:54 -0300

    Criando a pasta controllers

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 const express = require('express')
 
 const app = express();
+app.use(express.json());
+
+
+app.get("/", (request, response) => {
+    const { name, email, password } = request.body;
+    
+    response.json({ name, email, password })
+})
 
 const PORT = 3000;
-
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
