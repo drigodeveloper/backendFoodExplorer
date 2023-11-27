98f9936edb081c66ce7d5dc29d5a15e39f8bd0fb
Rodrigo <drigodeveloper@gmail.com>
2023-11-26 22:54:49 -0300

    Criando migrate do menu

--- src/database/knex/migrations/20231127014955_menu.js
+++ src/database/knex/migrations/20231127014955_menu.js
@@ -1,15 +1,14 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+exports.up = knex => knex.schema.createTable("menu", table => {
+    table.increments("id");
+    table.text("avatar");
+    table.text("title");
+    table.text("description");
+    table.text("category");
+
+    table.timestamp("created_at").default(knex.fn.now());
+    table.timestamp("updated_at").default(knex.fn.now());
+});
+
+
+exports.down = knex => knex.schema.dropTable("menu");
