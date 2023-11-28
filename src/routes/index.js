a9015eae66fca538c2e24385d7813cc152e2dcdc
Rodrigo <drigodeveloper@gmail.com>
2023-11-27 22:40:03 -0300

    Refatorando migrations

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,9 @@
 const { Router } = require("express");
 
 const userRouter = require('./users.routes')
-const dishesRouter = require('./dishes.routes')
 
 const routes = Router();
 
 routes.use("/users", userRouter);
-routes.use("/dishes", dishesRouter);
 
 module.exports = routes;
